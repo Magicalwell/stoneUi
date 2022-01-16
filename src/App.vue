75b9c381895dcf6a20a8b6c21a7317f103688ba4
threestone <812081428@qq.com>
2022-01-16 22:51:23 +0800

    feat:完成滚动指令v1.0(待完善嵌套情况)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div
     id="app"
     v-water-mark="{
-      text: 'lzg版权所有',
+      text: '3srstudio.cc',
       textColor: 'rgba(180, 180, 180, 0.4)',
     }"
   >
@@ -12,6 +12,7 @@
       </button>
       <input type="text" v-throttle="[inputest, 'input', 1000]" />
       <st-todolist></st-todolist>
+      <div class="test" id="test">909909321</div>
       <img
         v-lazy-img="{
           url: 'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/ed44c6756d7b4c4989e9623baad731b9~tplv-k3u1fbpfcp-image.image',
@@ -20,10 +21,11 @@
         alt=""
         style="width: 100px; height: 100px; margin-top: 600px"
       />
+      <hr />
       <div
         v-scroll-to="{
-          container: 'body',
-          el: '#testbottom',
+          container: '#app',
+          el: '#test',
           direction: 'y',
           during: 300,
           node: $refs.testbottom,
@@ -56,16 +58,22 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 600px;
+  overflow: auto;
   .test-box {
     width: 500px;
-    height: 700px;
+    height: 1200px;
     border: 1px solid #999;
+    overflow: auto;
   }
 }
 </style>
--- src/directives/src/scrollto.js
+++ src/directives/src/scrollto.js
@@ -1,20 +1,23 @@
 const scrollto = {
   name: "scroll-to",
-  abort: true,
-  timestart: null,
-  timestamp: null,
+  abort: false, //控制是否退出
+  timestart: null, //开始时间
+  timestamp: null, //时间戳
   container: null, // 包裹内容的盒子，要是可以滚动的 el为目标元素
   options: {
+    //配置项
     text: "3132",
   },
+  initialX: null, //初始X坐标
+  initialY: null, //初始Y坐标
+  targetX: null, //目标X坐标
+  targetY: null, //目标Y坐标
+  target: null, //目标元素
+  process: null, //比例
   bind: function (el, binding) {
-    console.log(el, binding);
     // 这里优化一下 用解构赋值过滤一下传递的参数，对于必填项没有填的报错
     scrollto.options = Object.assign({}, scrollto.options, binding.value);
-    console.log(scrollto.options);
-    console.log(scrollto.options.el);
-    console.log(document.querySelector(scrollto.options.el));
-    scrollto.container = document.querySelector(scrollto.options.el);
+    scrollto.container = document.querySelector(scrollto.options.container);
     el.addEventListener("click", scrollto.goScroll);
   },
   unbind: function (el) {
@@ -22,51 +25,73 @@ const scrollto = {
     el.removeEventListener("click", scrollto.goScroll);
   },
   goScroll(e) {
-    console.log(document.querySelector(scrollto.options.el));
     e.preventDefault();
-    scrollTopDistance(scrollto.container);
-    scrollLeftDistance(scrollto.container);
+    scrollto.abort = false;
+    scrollto.timestart = null;
+    scrollto.container = document.querySelector(scrollto.options.container);
+    scrollto.target = document.querySelector(scrollto.options.el);
+    scrollto.initialY = scrollTopDistance(scrollto.container, true);
+    scrollto.initialX = scrollLeftDistance(scrollto.container, true);
+    scrollto.targetY = scrollTopDistance(scrollto.target, false);
+    scrollto.targetX = scrollLeftDistance(scrollto.target, false);
+    // scrollto.container.scrollTo({ top: 0, behavior: "smooth" });
     window.requestAnimationFrame(scrollStep);
   },
 };
-function scrollTopDistance(container) {
-  console.log(container);
-  let scrollTop = container.scrollTop;
+const cubic = (value) => Math.pow(value, 3);
+const easeInOutCubic = (value) =>
+  value < 0.5 ? cubic(value * 2) / 2 : 1 - cubic((1 - value) * 2) / 2;
+
+function scrollTopDistance(container, flag = true) {
+  let scrollTop = flag ? container.scrollTop : container.offsetTop;
   if (container.tagName.toLowerCase() == "body") {
     scrollTop = scrollTop || document.documentElement.scrollTop;
   }
   return scrollTop;
 }
-function scrollLeftDistance(container) {
-  let scrollLeft = container.scrollLeft;
+function scrollLeftDistance(container, flag) {
+  let scrollLeft = flag ? container.scrollLeft : container.offsetLeft;
   if (container.tagName.toLowerCase() == "body") {
     scrollLeft = scrollLeft || document.documentElement.scrollLeft;
   }
   return scrollLeft;
 }
 function scrollStep(timestamp) {
+  // 暂时只考虑了父盒子有滚动条，目标盒子为父盒子的直接子盒子，需要考虑嵌套情况
   if (scrollto.abort) return;
   if (!scrollto.timestart) scrollto.timestart = timestamp;
-  console.log(timestamp);
-  move();
-  timestamp - scrollto.timestart < scrollto.options.during
+  let progress = easeInOutCubic(
+    (timestamp - scrollto.timestart) / scrollto.options.during
+  ).toFixed(2);
+  // console.log(progress);
+  move(
+    scrollto.container,
+    scrollto.initialY + (scrollto.targetY - scrollto.initialY) * progress,
+    scrollto.initialX + (scrollto.targetX - scrollto.initialX) * progress
+  );
+  // timestamp - scrollto.timestart < scrollto.options.during
+  //   ? window.requestAnimationFrame(scrollStep)
+  //   : done();
+  (timestamp - scrollto.timestart) / scrollto.options.during < 1
     ? window.requestAnimationFrame(scrollStep)
     : done();
 }
-function move() {
-  console.log(77777);
+function move(element, top, left) {
+  if (scrollto.options.direction == "y") element.scrollTop = top;
+  if (scrollto.options.direction == "x") element.scrollLeft = left;
+  if (element.tagName.toLowerCase() === "body") {
+    // in firefox body.scrollTop doesn't scroll the page
+    // thus if we are trying to scrollTop on a body tag
+    // we need to scroll on the documentElement
+    if (scrollto.options.direction == "y")
+      document.documentElement.scrollTop = top;
+    if (scrollto.options.direction == "x")
+      document.documentElement.scrollLeft = left;
+  }
 }
-function done() {}
-// on(element, events, handler, opts = { passive: false }) {
-//     if (!(events instanceof Array)) {
-//       events = [events]
-//     }
-//     for (let i = 0; i < events.length; i++) {
-//       element.addEventListener(
-//         events[i],
-//         handler,
-//         supportsPassive ? opts : false
-//       )
-//     }
-//   },
+function done() {
+  scrollto.container.scrollTop = scrollto.targetY;
+  // console.log(scrollto.target.scrollTop);
+  scrollto.abort = true;
+}
 export default scrollto;
